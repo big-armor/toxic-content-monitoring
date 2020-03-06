c1f6455f0da12739bc731e9ad93174ef490feb93
mljarman <maggiejarman2@gmail.com>
2020-03-06 16:39:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 """
-Main  
+Main
 """
 import re
 import string
 from fastapi import FastAPI
-from pydantic import BaseModel
 import sklearn
 from sklearn.externals import joblib
+from pydantic import BaseModel
 
 
 model = joblib.load('compressed_pipeline.pkl')
@@ -21,158 +21,161 @@ class Data(BaseModel):
 @app.post("/predict/")
 async def predict(data: Data):
     """
-    This API accepts string based requests and cleans the text. 
+    This API accepts string based requests and cleans the text.
 
-    Example Request Body:
-
-    {"text": "string"}
-    
     The following are removed:
-    - Newline characters 
+    - Newline characters
     - Return characters
     - Leading and trailing white spaces
     - Usernames if indicated with the term 'User'
     - IP addresses and user IDs
     - Non-printable characters e.g. unicode
 
-    **Model**: Term frequency-inverse document frequency(Tfidf) and Logistic Regression
-    
-    
-    The model is used to determine if the text contains toxic or offensive content. 
-    The possible labels are toxic, severe toxic, obscene, threat, insult, and identity 
-    hate. If no toxic or obscene content is detected the result will be, "No toxic or 
-    offensive content detected." 
+    Request URL:
+    https://fast-api-test2.appspot.com/predict/
 
-    The API returns the original text, any classifications that apply, and the predicted 
+    Example Request Body:
+
+    {"text": "string"}
+
+    **Model**: Term frequency-inverse document frequency(Tfidf) and Logistic Regression
+
+
+    The model is used to determine if the text contains toxic or offensive content.
+    The possible labels are toxic, severe toxic, obscene, threat, insult, and identity
+    hate. If no toxic or obscene content is detected the result will be, "No toxic or
+    offensive content detected."
+
+    The API returns the original text, any classifications that apply, and the predicted
     probability of the labels given.
 
     The current model is for testing purposes only, and will be replaced by a more robust natural language processing
-    model that has a higher positive recall score for each class.
+    model that has a higher positive recall score for each class.Recall demonstrates how effectively a model identifies
+    true positives, or in this case, toxicity. A low recall score would indicate a substantial amount of toxic content
+    is classified as non-toxic. Next versions of the model will work to minimize false negatives to ensure potentially
+    dangerous content is not missed.
 
-    
-    |**Toxic**      |precision    |recall  |f1-score   |support|
-    |:----------:|:-------:|:-------:|:-------:|:-------:|
-    | negative   | 0.97    | 0.99    | 0.98    | 28927   |
-    |  positive    |  0.85   |   0.68  |    0.75 | 2988    |
-    |**accuracy**|         |         | 0.96    | 31915   |
-    |**macro avg**|    0.91 |   0.83  |    0.86 |   31915 |
-    |**weighted avg**|    0.96 |   0.96  |    0.96 |   31915 |
-
-    --------
-
-
-    |**Severe Toxic**|precision    |recall  |f1-score   |support|
-    |:----------:|:-------:|:-------:|:-------:|:-------:|
-    | negative      |  0.99   |  1.00    | 1.00   | 31612   |
-    |    positive        |0.53     |0.29     |0.38     |  303  |
-    |**accuracy**|         |         | 0.99    | 31915   |
-    |**macro avg**|    0.76 |   0.65  |    0.69 |   31915 |
-    |**weighted avg**|    0.99 |   0.99 |    0.99|   31915 |
-
-    --------
-
-
-    |**Obscene**   |precision    |recall  |f1-score   |support|
-    |:----------:|:-------:|:-------:|:-------:|:-------:|
-    | negative     |  0.98   |  0.99      | 0.99     | 30291  |
-    |   positive      |0.88     |0.71      |0.79     |  1624 |
-    |**accuracy**|         |         | 0.98    | 31915   |
-    |**macro avg**|    0.93 |   0.85  |    0.89 |   31915 |
-    |**weighted avg**|    0.98 |   0.98 |    0.98|   31915 |
-
-
-    --------
-
-
-    |**Threat**    |precision    |recall  |f1-score   |support|
-    |:----------:|:-------:|:-------:|:-------:|:-------:|
-    | negative     | 1.00    | 1.00   | 1.00    | 31825   |
-    |    positive       |  0.63   |   0.27  |    0.38 | 90    |
-    |**accuracy**|         |         | 0.96    | 31915   |
-    |**macro avg**|    0.81 |   0.63  |    0.69 |   31915 |
-    |**weighted avg**|    1.00 |   1.00 |    1.00 |   31915 |
-
-
-    --------
-
-
-    |**Insult**  |precision    |recall  |f1-score   |support|
-    |:----------:|:-------:|:-------:|:-------:|:-------:|
-    |negative       | 0.98    | 0.99    | 0.99    | 30412   |
-    |    positive         |  0.76   |   0.56  |    0.65 | 1503    |
-    |**accuracy**|         |         | 0.97    | 31915   |
-    |**macro avg**|    0.87 |   0.78  |    0.82 |   31915 |
-    |**weighted avg**|    0.97 |   0.97  |    0.97 |   31915 |
-
-
-    --------
-
-
-    |**Identity Hate**      |precision    |recall  |f1-score   |support|
-    |:----------:|:-------:|:-------:|:-------:|:-------:|
-    | negative     | 0.99    | 1.00    | 1.00    | 31638   |
-    |  positive      |  0.60   |   0.26  |    0.36 | 277    |
-    |**accuracy**|         |         | 0.99    | 31915   |
-    |**macro avg**|    0.79 |   0.63  |    0.68 |   31915 |
-    |**weighted avg**|    0.99 |   0.99  |    0.99 |   31915 |
-
+    |**Class**    |**Recall Score**|
+    |:-----------:|:--------------:|
+    |Toxic        |.68             |
+    |Severe Toxic |.29             |
+    |Obscene      |.71             |
+    |Threat       |.27             |
+    |Insult       |.56             |
+    |Identity Hate|.26             |
     """
-    
+
+
     data_dict = data.dict()
-    
+
     # clean text to remove characters and metadata that may interfere with accuracy of model
     text = clean_text(data.text)
-    
+
     # model requires text be put into a list to function
     text = [text]
-    
+
     # use unpickled model to make prediction
     prediction = model.predict(text).tolist()[0]
-    
+
     # identify labels to process predictions
-    labels = ["toxic", "severe toxic", "obscene", "threat", "insult", "identity hate"] 
-    
+    labels = ["toxic", "severe toxic", "obscene", "threat", "insult", "identity hate"]
+
     # process predictions to match labels
     results = [label for i, label in enumerate(labels) if prediction[i]]
-    
+
     # get prediction probabalities for each possible label
-    pred_probabilities = model.predict_proba(text).tolist()[0]
-    
+    pred_probs = model.predict_proba(text).tolist()[0]
+
     # get prediction probabilities just for labels that apply
-    probabilty = [pred_probability for i, pred_probability in enumerate(pred_probabilities) if prediction[i]]
-   
+    probabilty = [round(pred_prob, 2) for i, pred_prob in enumerate(pred_probs) if prediction[i]]
+
     # for text that is not labeled provide a result to indicate no class identification
     if len(results) == 0:
         results = ["No toxic or offensive content detected"]
-    
+
     # return results
     return {"text": text, "prediction": results, "probability": probabilty}
+
+@app.post("/classify")
+async def classify(data: Data):
+    """
+    This is the same as the predict method, with some formatting differences.
+    New output will be in the following format:
+
+    {
+    "text": "stupid you're the worst",
+    "results": [
+    {
+      "prediction": "toxic",
+      "probability": 1
+    },
+    {
+      "prediction": "insult",
+      "probability": 0.92}]}
+     Recall is being used as an accuracy metric and the scores below reflect the percentage
+     of positively identied instances of these classes.
+    """
+    data_dict = data.dict()
+
+    # clean text to remove characters and metadata that may interfere with accuracy of model
+    text = clean_text(data.text)
+
+    # model requires text be put into a list to function
+    text = [text]
+
+    # FORMATTING CHANGE: removes string from array for output
+    display_text = " ".join(str(i) for i in text)
+
+    # use unpickled model to make prediction
+    prediction = model.predict(text).tolist()[0]
+
+    # identify labels to process predictions
+    labels = ['toxic', 'severe toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+
+    # process predictions to match labels
+    results = [label for i, label in enumerate(labels) if prediction[i]]
+
+    # get prediction probabalities for each possible label
+    pred_probs = model.predict_proba(text).tolist()[0]
+
+    # get prediction probabilities just for labels that apply
+    probability = [round(pred_prob, 2) for i, pred_prob in enumerate(pred_probs) if prediction[i]]
+
+    # FORMATTING CHANGE: creates 1 array that contains prediction and probability for each class detected
+    display_results = [{'prediction':label, 'probability':proba} for label,proba in zip(results, probability)]
+
+    # for text that is not labeled provide a result to indicate no class identification
+    if len(display_results) == 0:
+        display_results = ["No toxic or offensive content detected"]
+
+    # return results
+    return {"text": display_text, "results": display_results}
 
 
 def clean_text(x):
     """
-    Function for cleaning text to remove characters, user identification, and non-printable 
+    Function for cleaning text to remove characters, user identification, and non-printable
     characters that can interfer with the model's ability to make accurate predictions.
     """
     # remove newline characters
     x = re.sub('\\n',' ',x)
-    
+
     # remove return characters
     x = re.sub('\\r',' ',x)
-    
+
     # remove leading and trailing white space
     x = x.strip()
-    
-    # remove any text starting with User... 
+
+    # remove any text starting with User...
     x = re.sub("\[\[User.*", ' ', x)
-    
+
     # remove IP addresses or user IDs
     x = re.sub("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", ' ', x)
-    
+
     # remove URLs
     x = re.sub("(http://.*?\s)|(http://.*)", ' ', x)
-   
+
     # remove non_printable characters eg unicode
     x = "".join(list(filter(lambda c: c in printable, x)))
-    
+
     return x
