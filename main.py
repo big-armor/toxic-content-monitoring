0b0decc1c59f21d9c7796fff757084fe30a73d7c
mljarman <maggiejarman2@gmail.com>
2020-03-25 16:03:17 -0600

    add comments to text and update API documentation
    Added comments for code to:
    * main.py
    * model.py
    * preprocessor.py
    
    Also updated documentation for API in main.py (please provide feedback with anything and in particular the scoring portion of API documentation.)

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ from pydantic import BaseModel
 import torch
 from torch.autograd import Variable
 from torch.nn.utils.rnn import pad_sequence
+
 from model import ToxicClassifierModel, vectors
 from preprocessor import normalize_comment, clean_text
 
 
-
+# load model
 model = ToxicClassifierModel()
 model.load_state_dict(torch.load("data/TCM_2.pt"))
 model.eval()
 
 
-
 app = FastAPI()
 
+# makes request body rather than parameter
 class Data(BaseModel):
     text: str
 
@@ -27,8 +28,10 @@ class Data(BaseModel):
 @app.post("/predict/")
 async def predict(data: Data):
     """
-    This API accepts string based requests and cleans the text before running it through the prediction model.
+    This API accepts string based requests and preprocesses the text before
+    running it through the predictive model.
 
+    **Preprocessing**
     The following are removed:
     - Newline characters
     - Return characters
@@ -37,6 +40,14 @@ async def predict(data: Data):
     - IP addresses and user IDs
     - Non-printable characters e.g. unicode
 
+    Words that are disguised using characters such as * or @ are replaced with
+    letters and common astericks words are replaced with coordinating word.
+
+    The API also preprocesses text so that if toxic words are mixed in with
+    non-toxic content, it will split the text before and after so the toxic words
+    are identified.
+
+    **Making Requests**
     Request URL:
     https://fast-api-test2.appspot.com/predict/
 
@@ -44,63 +55,70 @@ async def predict(data: Data):
 
     {"text": "string"}
 
-    **Model**: Term frequency-inverse document frequency(Tfidf) and Logistic Regression
-
-
+    **Model**
     The model is used to determine if the text contains toxic or offensive content.
-    
+
     The possible labels are:
     - toxic
-    - severe 
-    - toxic 
-    - obscene 
-    - threat 
-    - insult 
+    - severe toxic
+    - obscene
+    - threat
+    - insult
     - identity hate
 
-    The API returns the original text, any classifications that apply, and the predicted
-    probability of the labels given.
+    The API returns the cleaned text, all labels, True of False for each label,
+    and the predicted probability of each.
 
-    The current model is for testing purposes only, and will be replaced by a more robust natural language processing
-    model that has a higher positive recall score for each class. Recall demonstrates how effectively a model identifies
-    true positives, or in this case, toxicity. A low recall score would indicate a substantial amount of toxic content
-    is classified as non-toxic. Next versions of the model will work to minimize false negatives to ensure potentially
-    dangerous content is not missed.
+    The current model is a Bi-directional LSTM + GRU neural network made with
+    PyTorch, assuming FastText vectorization. Considerable preprocessing is
+    performed on the text before vectorization. The metrics used in evaluating
+    this model are F1 and ROC-AUC scores.
 
+    F1 score is defined as the harmonic mean between precision and recall on a scale of
+    0 to 1. Recall demonstrates how effectively this model identifies all relevant
+    instances of toxicity. Precision demonstrates how effectively the model returns
+    only these relevant instances.
+
+    The AUC score represents the measure of separability, in this case, distinguishing
+    between toxic and non-toxic content. Also on a scale of 0 to 1, a high AUC score
+    indicates the model successfully classifies toxic vs non-toxic. The ROC represents
+    the probability curve.
+
+    The F1 score for this model is 0.753 and the ROC-AUC score is 0.987
     """
-
-
-    data_dict = data.dict()
 
     # clean text to remove characters and metadata that may interfere with accuracy of model
     text = clean_text(data.text)
 
-    # normalize comment
+    # normalize comment (see preprocessor.py file)
     normalized_words = normalize_comment(text)
 
+    # vectorize the normalized comment using FastText
     var_vectorized_sentence = Variable(torch.nn.utils.rnn.pad_sequence([vectors[normalized_words]]).permute(1,0,2)).float()
 
+    # creates predictions for text on different labels
     output = model(var_vectorized_sentence)
 
+    # removes prediction from tensor object and into a list
     predProb = output.data.numpy().tolist()[0]
 
+    # threshold can be changed in order to increase positive recall scores
     threshold = 0.5
     preds = []
-    
+    # returns true or false for each class based on prediction
     for p in predProb:
         if p >= threshold:
             preds.append(True)
-        else: 
+        else:
             preds.append(False)
-    
 
     # identify labels to process predictions
     labels = ['toxic', 'severe toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 
-    # FORMATTING CHANGE: creates 1 array that contains prediction and probability for each class detected
+    # returns for each class: boolean classification and predicted probability
     display_results = {label:{'prediction':pred, 'probability':round(proba, 5)} for label,pred,proba in zip(labels, preds, predProb)}
 
-    # return results
+    # returns cleaned input text and display_results
     return {"text": text, "results": display_results}
 
 
--- preprocessor.py
+++ preprocessor.py
@@ -9,7 +9,13 @@ from nltk.tokenize import TweetTokenizer
 
 import sys
 
-wordFiles = ["data/identity_hateWordFile.txt", "data/insultWordFile.txt", "data/threatWordFile.txt", "data/toxicWordFile.txt", "data/obsceneWordFile.txt", "data/severe_toxicWordFile.txt"]
+# files of top 100 toxic words associated with each label
+wordFiles = ["data/identity_hateWordFile.txt",
+             "data/insultWordFile.txt",
+             "data/threatWordFile.txt",
+             "data/toxicWordFile.txt",
+             "data/obsceneWordFile.txt",
+             "data/severe_toxicWordFile.txt"]
 
 toxic_words = set([])
 
@@ -21,29 +27,32 @@ valid_characters = " " + "@$" + "'!?-" + "abcdefghijklmnopqrstuvwxyz" + "abcdefg
 valid_set = set(x for x in valid_characters)
 
 astericks_words = [
-                   ('mother****ers', 'motherfuckers'), ('motherf*cking', 'motherfucking'), ('mother****er', 'motherfucker'), ('motherf*cker', 'motherfucker'), 
-                   ('bullsh*t', 'bullshit'), ('f**cking', 'fucking'), ('f*ucking', 'fucking'), ('fu*cking', 'fucking'), ('****ing', 'fucking'), 
-                   ('a**hole', 'asshole'), ('assh*le', 'asshole'), ('f******', 'fucking'), ('f*****g', 'fucking'), ('f***ing', 'fucking'), 
+                   ('mother****ers', 'motherfuckers'), ('motherf*cking', 'motherfucking'), ('mother****er', 'motherfucker'), ('motherf*cker', 'motherfucker'),
+                   ('bullsh*t', 'bullshit'), ('f**cking', 'fucking'), ('f*ucking', 'fucking'), ('fu*cking', 'fucking'), ('****ing', 'fucking'),
+                   ('a**hole', 'asshole'), ('assh*le', 'asshole'), ('f******', 'fucking'), ('f*****g', 'fucking'), ('f***ing', 'fucking'),
                    ('f**king', 'fucking'), ('f*cking', 'fucking'), ('fu**ing', 'fucking'), ('fu*king', 'fucking'), ('fuc*ers', 'fuckers'),
-                   ('f*****', 'fucking'), ('f***ed', 'fucked'), ('f**ker', 'fucker'), ('f*cked', 'fucked'), ('f*cker', 'fucker'), ('f*ckin', 'fucking'), 
-                   ('fu*ker', 'fucker'), ('fuc**n', 'fucking'), ('ni**as', 'niggas'), ('b**ch', 'bitch'), ('b*tch', 'bitch'), ('c*unt', 'cunt'), 
-                   ('f**ks', 'fucks'), ('f*ing', 'fucking'), ('ni**a', 'nigga'), ('c*ck', 'cock'), ('c*nt', 'cunt'), ('cr*p', 'crap'), ('d*ck', 'dick'), 
-                   ('f***', 'fuck'), ('f**k', 'fuck'), ('f*ck', 'fuck'), ('fc*k', 'fuck'), ('fu**', 'fuck'), ('fu*k', 'fuck'), ('s***', 'shit'), 
+                   ('f*****', 'fucking'), ('f***ed', 'fucked'), ('f**ker', 'fucker'), ('f*cked', 'fucked'), ('f*cker', 'fucker'), ('f*ckin', 'fucking'),
+                   ('fu*ker', 'fucker'), ('fuc**n', 'fucking'), ('ni**as', 'niggas'), ('b**ch', 'bitch'), ('b*tch', 'bitch'), ('c*unt', 'cunt'),
+                   ('f**ks', 'fucks'), ('f*ing', 'fucking'), ('ni**a', 'nigga'), ('c*ck', 'cock'), ('c*nt', 'cunt'), ('cr*p', 'crap'), ('d*ck', 'dick'),
+                   ('f***', 'fuck'), ('f**k', 'fuck'), ('f*ck', 'fuck'), ('fc*k', 'fuck'), ('fu**', 'fuck'), ('fu*k', 'fuck'), ('s***', 'shit'),
                    ('s**t', 'shit'), ('sh**', 'shit'), ('sh*t', 'shit'), ('tw*t', 'twat')]
 
+# creates set of toxic words from all word_files
 for link in wordFiles:
     word_file = open(link, "r")
     toxic_words = toxic_words.union(set(word_file.read().split("\n")))
 
-# Only include words with length greater than or equal to 4
+# only include words with length greater than or equal to 4
 toxic_words = set(filter(lambda x: len(x) >= 4, toxic_words))
 
+# creates a set of printable characters
 printable = set(string.printable)
 
 def clean_text(x):
     """
-    Function for cleaning text to remove characters, user identification, and non-printable
-    characters that can interfer with the model's ability to make accurate predictions.
+    Function for cleaning text to remove characters, user identification, and
+    non-printable characters that can interfere with the model's ability to make
+    accurate predictions.
     """
     # remove newline characters
     x = re.sub('\\n',' ',x)
@@ -69,62 +78,76 @@ def clean_text(x):
     return x
 
 def split_word(word, toxic_words):
+    """
+    Function that accounts for toxic words being hidden within normal text. If
+    any of the toxic words are found, it will split the non-toxic text, exposing
+    the toxic word.
+    """
     if word == "":
         return ""
-    
+
     lower = word.lower()
     for toxic_word in toxic_words:
         lower = lower.replace(toxic_word, " " + toxic_word + " ")
-        
+
     return lower
 
 tknzr = TweetTokenizer(strip_handles=False, reduce_len=True)
 
 def word_tokenize(sentence):
+    """
+    Function that tokenizes text and replaces associated characters with letters
+    as well as adds spaces around punctuation.
+    """
     sentence = sentence.replace("$", "s")
-    sentence = sentence.replace("@", "a")    
+    sentence = sentence.replace("@", "a")
     sentence = sentence.replace("!", " ! ")
     sentence = sentence.replace("?", " ? ")
-    
+
     return tknzr.tokenize(sentence)
 
 def normalize_comment(comment):
+    """
+    Function that calls split_word and word_tokenize functions on the text so it
+    is in the necessary format to run through the model by creating list of words.
+    """
     comment = unidecode(comment)
     comment = comment[:length_threshold]
-    
+
     normalized_words = []
-    
+
+    # replaces with associated word while maintaining uppercase, if needed.
+    # example: W*rd becomes Word
     for w in astericks_words:
         if w[0] in comment:
             comment = comment.replace(w[0], w[1])
         if w[0].upper() in comment:
             comment = comment.replace(w[0].upper(), w[1].upper())
-    
-    for word in word_tokenize(comment):
-        #for (pattern, repl) in patterns:
-        #    word = re.sub(pattern, repl, word)
 
+    # places spaces around .'s and ,'s if needed and exposes toxic words if hidden
+    # example: bzt is a toxic word, 'Well bztuk you' becomes 'Well bzt uk you'
+    for word in word_tokenize(comment):
         if word == "." or word == ",":
             normalized_words.append(word)
             continue
-        
+
         if word.count(".") == 1:
             word = word.replace(".", " ")
         filtered_word = "".join([x for x in word if x in valid_set])
-                    
-        #For every word check if it has a toxic word as a part of it
-        #If so, split this word by swear and non-swear part.
+
         normalized_word = split_word(filtered_word, toxic_words)
 
         normalized_words.append(normalized_word)
-        
+
     normalized_comment = " ".join(normalized_words)
-    
+
     result = []
+
+    # maintains uppercase, if needed, and all else is lowercase
     for word in normalized_comment.split():
         if word.upper() == word:
             result.append(word)
         else:
             result.append(word.lower())
-    
+
     return result
